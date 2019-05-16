ae9188a943de08502488d9c9cfda4324ff04721d
여영호 <yyh3829@naver.com>
2019-05-16 17:08:06 +0900

    separation draw_target
    separation draw_target function other file

--- colorblindness.py
+++ colorblindness.py
@@ -1,9 +1,8 @@
-import math
-import random
 import sys
+import math
 
+from draw_target import *
 from PIL import Image, ImageDraw
-from colorset import *
 
 try:
     from scipy.spatial import cKDTree as KDTree
@@ -12,7 +11,6 @@ try:
 except ImportError:
     IMPORTED_SCIPY = False
 
-BACKGROUND = (255, 255, 255)
 TOTAL_CIRCLES = 1500
 
 
@@ -33,76 +31,11 @@ def generate_circle(image_width, image_height, min_diameter, max_diameter):
     return x, y, radius
 
 
-def overlaps_motive(image, x_y_r):
-    # 원의 겹침을 확인하는 함수, 반환값 : boolean
-    x, y, r = x_y_r
-    points_x = [x, x, x, x-r, x+r, x-r*0.93, x-r*0.93, x+r*0.93, x+r*0.93]
-    points_y = [y, y-r, y+r, y, y, y+r*0.93, y-r*0.93, y+r*0.93, y-r*0.93]
-
-    for xy in zip(points_x, points_y):
-        if image.getpixel(xy)[:3] != BACKGROUND:
-            return True
-        # 배열[행:열] 앞의 예는 3열을 고정시킨것, getpixel() 해당 점의 RGB 값 반환
-
-    return False
-
-
 def circle_intersection(x1_y1_r1, x2_y2_r2):
     # 두원의 겹침 조사 함수 겹치지 않을 때 true 반환, 반환값 : boolean
     x1, y1, r1 = x1_y1_r1
     x2, y2, r2 = x2_y2_r2
     return (x2 - x1)**2 + (y2 - y1)**2 < (r2 + r1)**2
-
-
-def circle_draw_target_1st(draw_image, image, x_y_r):
-    # 이미지에 원을 그려주는 함수
-    x, y, r = x_y_r
-    fill_colors = COLORS_ON_1 if overlaps_motive(image, (x, y, r)) else COLORS_OFF_1
-    fill_color = random.choice(fill_colors)
-    draw_image.ellipse((x - r, y - r, x + r, y + r),
-                       fill=fill_color,
-                       outline=fill_color)
-    # 원 생성 메소드
-
-
-def circle_draw_target_2nd(draw_image, image, image2, x_y_r):
-    # 제 2표 그려주는 함수
-    x, y, r = x_y_r
-    if overlaps_motive(image, (x, y, r)):
-        if overlaps_motive(image2, (x, y, r)):
-            fill_colors = COLORS_ON_2_1
-        else:
-            fill_colors = COLORS_ON_2_2
-    elif overlaps_motive(image2, (x, y, r)):
-        fill_colors = COLORS_ON_2_3
-    else:
-        fill_colors = COLORS_OFF_2
-
-    fill_color = random.choice(fill_colors)
-    draw_image.ellipse((x - r, y - r, x + r, y + r),
-                       fill=fill_color,
-                       outline=fill_color)
-    # 원 생성 메소드
-
-
-def circle_draw_target_11th(draw_image, image, image2, x_y_r):
-    # 제 11표 그려주는 함수
-    x, y, r = x_y_r
-    if overlaps_motive(image, (x, y, r)):
-        if overlaps_motive(image2, (x, y, r)):
-            fill_colors = COLORS_ON_11_1
-        else:
-            fill_colors = COLORS_ON_11_2
-    elif overlaps_motive(image2, (x, y, r)):
-        fill_colors = COLORS_ON_11_3
-    else:
-        fill_colors = COLORS_OFF_11
-
-    fill_color = random.choice(fill_colors)
-    draw_image.ellipse((x - r, y - r, x + r, y + r),
-                       fill=fill_color,
-                       outline=fill_color)
-    # 원 생성 메소드
 
 
 first_flag = True
