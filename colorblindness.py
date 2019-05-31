3255d7494b6f34c334109a6253a6f54af5e74630
여영호 <yyh3829@naver.com>
2019-05-31 16:24:40 +0900

    solution 1~12 complete
    solution 1~12 check colorblindness complete

--- colorblindness.py
+++ colorblindness.py
@@ -85,7 +85,8 @@ def bounded_check(image, x_y_r):
     return True
 
 
-dic = {'first': 1, 'second': 2, 'overlap': 3, 'second overlap': 4}
+dic = {'first': 1, 'second': 2, 'overlap': 3, 'second overlap': 4, 'filter': 3}
+sol_table = [0] * 10
 
 
 def shortcut(keyword):
@@ -128,38 +129,43 @@ def setting():
     return target_num, first_image
 
 
-def check_solution_print(a = 'x', b = 'x',  c = 'x', d = 'x'):
-        print('정상자 : ', a)
-        print('제 1, 제 2 색각 이상자 : ', b)
-        print('제 3 색각 이상자 : ', c)
-        print('전색약, 전색맹 : ', d)
+def check_solution_print(a='x', b='x', c='x', d='x'):
+    print('정상자 : ', a)
+    print('제 1, 제 2 색각 이상자 : ', b)
+    print('제 3 색각 이상자 : ', c)
+    print('전색약, 전색맹 : ', d)
 
 
 def power_solution_print(a, b, c):
-        print('정상자 : ', sol_table[1] + sol_table[2])
-        print('제 1, 제 2 색각 이상자 : ', sol_table[1] + sol_table[2])
-        print('제 3 색각 이상자 : ', sol_table[1] + sol_table[2])
-        print('전색약, 전색맹 : ', sol_table[1] + sol_table[2])
+    print('정상자 : ', sol_table[1] + sol_table[2])
+    print('제 1, 제 2 색각 이상자 : ', sol_table[1] + sol_table[2])
+    print('제 3 색각 이상자 : ', sol_table[1] + sol_table[2])
+    print('전색약, 전색맹 : ', sol_table[1] + sol_table[2])
+
 
 def solution():
     target_num = sol_table[0]
     print('제 ', target_num, '표')
     if target_num in [1]:
         check_solution_print(sol_table[1] + sol_table[2], sol_table[1] + sol_table[2], sol_table[1] + sol_table[2], sol_table[1] + sol_table[2])
-    elif target_num in [2]:
-        check_solution_print()
-    elif target_num in [4, 11]:
-        check_solution_print()
+    elif target_num in [2, 4]:
+        check_solution_print(sol_table[1], sol_table[3], sol_table[1])
     elif target_num in [3, 5]:
-        check_solution_print()
-    elif target_num in [6, 8, 12]:
-        check_solution_print()
-    elif target_num in [7, 10, 13, 14, 15, 16, 17, 18]:
-        check_solution_print()
+        check_solution_print(sol_table[1]+sol_table[2], sol_table[3] + sol_table[4], sol_table[1] + sol_table[2])
+    elif target_num in [6, 8]:
+        check_solution_print(sol_table[1], 'x', sol_table[1])
+    elif target_num in [7]:
+        check_solution_print(sol_table[1] + sol_table[2], 'x', sol_table[1] + sol_table[2])
     elif target_num in [9]:
-        check_solution_print()
+        check_solution_print('x', sol_table[1])
+    elif target_num in [10]:
+        check_solution_print(sol_table[1]+sol_table[2], sol_table[1]+sol_table[2])
+    elif target_num in [11]:
+        check_solution_print('긴 길', '지름길', '긴길')
+    elif target_num in [12]:
+        check_solution_print('o', 'x', 'o')
     elif target_num in [19, 20, 21]:
-        check_solution_print()
+        power_solution_print()
 
 
 def main():
