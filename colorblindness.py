a442bb6b895f84c99b738c58648f5e2153cd489a
hoonki <chaeefe@gmail.com>
2019-06-01 12:58:28 +0900

    divided view code

--- colorblindness.py
+++ colorblindness.py
@@ -3,7 +3,7 @@
 import sys
 import os
 import math
-
+from view import *
 from draw_target import *
 from make_solution import *
 from PyQt5 import QtCore, QtGui, QtWidgets
@@ -144,111 +144,7 @@ def setting():
         sys.exit()
     return target_num, first_image
 
-class Ui_MainWindow(QWidget):
-    def setupUi(self, MainWindow):
-        MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(693, 490)
-        MainWindow.setStyleSheet("QGroupBox {\n"
-                                    "    border: 1px solid gray;\n"
-                                    "    border-radius: 9px;\n"
-                                    "    margin-top: 0.5em;\n"
-                                    "}\n"
-                                    "\n"
-                                    "QGroupBox::title {\n"
-                                    "    subcontrol-origin: margin;\n"
-                                    "    left: 10px;\n"
-                                    "    padding: 0 3px 0 3px;\n"
-                                    "}")
-        self.centralwidget = QtWidgets.QWidget(MainWindow)
-        self.centralwidget.setObjectName("centralwidget")
-        self.groupBox = QtWidgets.QGroupBox(self.centralwidget)
-        self.groupBox.setEnabled(True)
-        self.groupBox.setGeometry(QtCore.QRect(20, 10, 331, 421))
-        self.groupBox.setStyleSheet("QGroupBox {\n"
-                                    "    border: 1px solid gray;\n"
-                                    "    border-radius: 9px;\n"
-                                    "    margin-top: 0.5em;\n"
-                                    "}\n"
-                                    "\n"
-                                    "QGroupBox::title {\n"
-                                    "    subcontrol-origin: margin;\n"
-                                    "    left: 10px;\n"
-                                    "    padding: 0 3px 0 3px;\n"
-                                    "}")
-        self.groupBox.setFlat(False)
-        self.groupBox.setCheckable(False)
-        self.groupBox.setObjectName("groupBox")
-        self.lblTargetNum = QtWidgets.QLabel(self.groupBox)
-        self.lblTargetNum.setGeometry(QtCore.QRect(10, 30, 64, 15))
-        self.lblTargetNum.setObjectName("lblTargetNum")
-        self.comboTarget = QtWidgets.QComboBox(self.groupBox)
-        self.comboTarget.setGeometry(QtCore.QRect(230, 30, 94, 22))
-        self.comboTarget.setObjectName("comboTarget")
-        for i in range(1,22):
-            self.comboTarget.addItem("")
-        self.lblFirstChar = QtWidgets.QLabel(self.groupBox)
-        self.lblFirstChar.setGeometry(QtCore.QRect(10, 80, 81, 16))
-        self.lblFirstChar.setObjectName("lblFirstChar")
-        self.txtfirst = QtWidgets.QTextBrowser(self.groupBox)
-        self.txtfirst.setGeometry(QtCore.QRect(10, 110, 311, 31))
-        self.txtfirst.setOpenExternalLinks(False)
-        self.txtfirst.setObjectName("txtfirst")
-        self.btnFirst = QtWidgets.QPushButton(self.groupBox)
-        self.btnFirst.setGeometry(QtCore.QRect(230, 80, 93, 28))
-        self.btnFirst.setObjectName("btnFirst")
-        self.btnFirstOver = QtWidgets.QPushButton(self.groupBox)
-        self.btnFirstOver.setGeometry(QtCore.QRect(230, 150, 93, 28))
-        self.btnFirstOver.setObjectName("btnFirstOver")
-        self.txtFirstOver = QtWidgets.QTextBrowser(self.groupBox)
-        self.txtFirstOver.setGeometry(QtCore.QRect(10, 180, 311, 31))
-        self.txtFirstOver.setOpenExternalLinks(False)
-        self.txtFirstOver.setObjectName("txtFirstOver")
-        self.lblFirstOver = QtWidgets.QLabel(self.groupBox)
-        self.lblFirstOver.setGeometry(QtCore.QRect(10, 150, 81, 16))
-        self.lblFirstOver.setObjectName("lblFirstOver")
-        self.lblSndOver = QtWidgets.QLabel(self.groupBox)
-        self.lblSndOver.setGeometry(QtCore.QRect(10, 300, 81, 16))
-        self.lblSndOver.setObjectName("lblSndOver")
-        self.txtSndOver = QtWidgets.QTextBrowser(self.groupBox)
-        self.txtSndOver.setGeometry(QtCore.QRect(10, 330, 311, 31))
-        self.txtSndOver.setOpenExternalLinks(False)
-        self.txtSndOver.setObjectName("txtSndOver")
-        self.btnSndOver = QtWidgets.QPushButton(self.groupBox)
-        self.btnSndOver.setGeometry(QtCore.QRect(230, 300, 93, 28))
-        self.btnSndOver.setObjectName("btnSndOver")
-        self.btnSnd = QtWidgets.QPushButton(self.groupBox)
-        self.btnSnd.setGeometry(QtCore.QRect(230, 230, 93, 28))
-        self.btnSnd.setObjectName("btnSnd")
-        self.txtSnd = QtWidgets.QTextBrowser(self.groupBox)
-        self.txtSnd.setGeometry(QtCore.QRect(10, 260, 311, 31))
-        self.txtSnd.setOpenExternalLinks(False)
-        self.txtSnd.setObjectName("txtSnd")
-        self.lblSndChar = QtWidgets.QLabel(self.groupBox)
-        self.lblSndChar.setGeometry(QtCore.QRect(10, 230, 81, 16))
-        self.lblSndChar.setObjectName("lblSndChar")
-        self.btnTrans = QtWidgets.QPushButton(self.groupBox)
-        self.btnTrans.setGeometry(QtCore.QRect(230, 380, 93, 28))
-        self.btnTrans.setObjectName("btnTrans")
-        self.groupBox_2 = QtWidgets.QGroupBox(self.centralwidget)
-        self.groupBox_2.setGeometry(QtCore.QRect(370, 10, 301, 421))
-        self.groupBox_2.setObjectName("groupBox_2")
-        self.btnSave = QtWidgets.QPushButton(self.groupBox_2)
-        self.btnSave.setGeometry(QtCore.QRect(200, 380, 93, 28))
-        self.btnSave.setObjectName("btnSave")
-        self.resultImage = QtWidgets.QLabel(self.groupBox_2)
-        self.resultImage.setGeometry(QtCore.QRect(10, 30, 280, 330))
-        self.resultImage.setObjectName("resultImage")
-        MainWindow.setCentralWidget(self.centralwidget)
-        self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 693, 26))
-        self.menubar.setObjectName("menubar")
-        MainWindow.setMenuBar(self.menubar)
-        self.statusbar = QtWidgets.QStatusBar(MainWindow)
-        self.statusbar.setObjectName("statusbar")
-        MainWindow.setStatusBar(self.statusbar)
-
-        self.retranslateUi(MainWindow)
-        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+class Ui_MainWindow(Ui_SubWindow):
 
     def selectionChanged(self):
         global target_num
@@ -384,34 +280,6 @@ class Ui_MainWindow(QWidget):
             os.makedirs(output_dir)
         image2.save(os.path.join(output_dir, 'new_colorblindness_sample.png'))
         print("저장되었습니다")
-
-    def retranslateUi(self, MainWindow):
-        _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.groupBox.setTitle(_translate("MainWindow", "setting"))
-        self.lblTargetNum.setText(_translate("MainWindow", "지표설정"))
-        for i in range(1,22):
-            self.comboTarget.setItemText(i - 1, _translate("MainWindow", str(i) ))
-        self.comboTarget.currentIndexChanged.connect(self.selectionChanged)
-        self.lblFirstChar.setText(_translate("MainWindow", "첫번째 문자"))
-        self.btnFirst.setText(_translate("MainWindow", "찾아보기"))
-        self.btnFirstOver.setText(_translate("MainWindow", "찾아보기"))
-        self.lblFirstOver.setText(_translate("MainWindow", "겹침 문자"))
-        self.lblSndOver.setText(_translate("MainWindow", "겹침 문자"))
-        self.btnSndOver.setText(_translate("MainWindow", "찾아보기"))
-        self.btnSnd.setText(_translate("MainWindow", "찾아보기"))
-        self.lblSndChar.setText(_translate("MainWindow", "두번째 문자"))
-        self.btnTrans.setText(_translate("MainWindow", "변환"))
-        self.groupBox_2.setTitle(_translate("MainWindow", "result"))
-        self.btnSave.setText(_translate("MainWindow", "저장"))
-        self.btnFirstOver.setEnabled(False)
-        self.btnSndOver.setEnabled(False)
-        self.btnFirst.clicked.connect(self.btnClickEventOpenFileDialog1)
-        self.btnFirstOver.clicked.connect(self.btnClickEventOpenFileDialog2)
-        self.btnSnd.clicked.connect(self.btnClickEventOpenFileDialog3)
-        self.btnSndOver.clicked.connect(self.btnClickEventOpenFileDialog4)
-        self.btnTrans.clicked.connect(self.btnClickEventTranspose)
-        self.btnSave.clicked.connect(self.btnClickEventSaveImage)
 
 
 def main():
